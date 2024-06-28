234f6ae58983b9d08a1fee94f3aca52f61a2c0b3
SniperKahn <143447320+SniperKahn@users.noreply.github.com>
2024-06-28 23:13:20 +0000

    First Commit  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	new file:   public/index.html
    	new file:   server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { exec } = require('child_process');
+const path = require('path');
+const os = require('os');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/download', (req, res) => {
+    const { url } = req.body;
+    if (!url) {
+        return res.status(400).send('URL is required');
+    }
+
+    const outputPath = path.join( os.homedir(), 'Downloads', '%(title)s.%(ext)s');
+    const command = `yt-dlp -f 'best[ext=mp4]' -o '${outputPath}' ${url}`;
+
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${stderr}`);
+            return res.status(500).send(`Error downloading video: ${stderr}`);
+        }
+        console.log(`Output: ${stdout}`);
+        res.send('Download started successfully');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
